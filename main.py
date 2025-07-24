e7243e0126e9d5e54aa974e896533db810477a81
Deidra Clay <dclay@bestsanitizers.com>
2025-07-24 07:09:23 -0400

    .exe built

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import gui.pages.start_page
+import gui.pages.parse_page
+import gui.pages.progress_page
+import gui.pages.folder_page
+import gui.pages.summary_page
 from gui.wizard import run
 
 if __name__ == "__main__":
